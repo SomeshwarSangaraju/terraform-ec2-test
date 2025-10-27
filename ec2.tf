d4630e9f60070b9abe063bccf2048e2012d5da27
SSR <someshwar.sangaraju19@gmail.com>
2025-10-27 20:25:43 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -5,3 +5,15 @@ module "catalogue"{
     sg_ids=var.sg_ids
     tags=var.tags
 }
+
+output "pub_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "priv_ip" {
+    value = module.catalogue.private_ip
+}
+
+output "id" {
+    value = module.catalogue.instance_id
+}
